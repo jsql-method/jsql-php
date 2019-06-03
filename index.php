88f089fe76deb669cd88303baf7c236cc2b9370f
Michał Poniecki <michal.poniecki@software-house.eu>
2019-06-03 15:53:27 +0200

    Aplikacja została przepisana na nowo

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+// Application's mounting point
+require_once "autoload.php";
+
+// Using clause
+use JSQL\Router;
+
+// Run router
+Router::run();
---
+++ router/Handler.php
@@ -0,0 +1,95 @@
+<?php
+namespace JSQL;
+
+/**
+ * Class Handler
+ * @package JSQL
+ */
+class Handler
+{
+    /**
+     * @var string Application url
+     */
+    public static $apiUri = "https://test-provider.jsql.it/api/jsql/";
+    public static $txId = "txid";
+
+    /**
+     * Retrieve the post request result
+     *
+     * @since       1.0.0
+     * @param       string      $path       Request endpoint
+     * @param       array|null  $headers    Request headers
+     * @return      void                    Parsed object
+     */
+    public static function request_post($path, $headers = NULL)
+    {
+        // POST content reading
+        $json = file_get_contents('php://input');
+
+        // TXID Append
+        $postHeaders = getallheaders();
+        $txName = self::$txId;
+        $txId = $postHeaders[$txName] ?? null;
+
+        // Prepare query string
+        $baseHeaders = array(
+            'Content-Type: application/json',
+            'Content-Length: ' . strlen($json)
+	    );
+
+        // If txid exists
+        if( $txId )
+        {
+            $baseHeaders[] = "{$txName}: {$txId}";
+        }
+
+        // Custom headers merge
+        if(isset($headers))
+        {
+            // Prepare headers string
+            foreach($headers as $header => $value)
+            {
+                $baseHeaders[] = "{$header}: $value";
+            }
+        }
+
+        // Curl handler
+        $handler = curl_init();
+        $responseHeaders = [];
+        curl_setopt($handler, CURLOPT_URL, self::$apiUri . $path);
+        curl_setopt($handler, CURLOPT_CUSTOMREQUEST, "POST");
+        curl_setopt($handler, CURLOPT_POSTFIELDS, $json);
+        curl_setopt($handler, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($handler, CURLOPT_HTTPHEADER, $baseHeaders);
+        curl_setopt($handler, CURLOPT_HEADERFUNCTION,
+            function($curl, $header) use (&$responseHeaders)
+            {
+                $len = strlen($header);
+                $header = explode(':', $header, 2);
+                if (count($header) < 2) // ignore invalid headers
+                    return $len;
+
+                $name = strtolower(trim($header[0]));
+                if (!array_key_exists($name, $responseHeaders))
+                    $responseHeaders[$name] = [trim($header[1])];
+                else
+                    $responseHeaders[$name][] = trim($header[1]);
+
+                return $len;
+            }
+        );
+
+        // Receive response object
+        $responseBody = curl_exec($handler);
+
+        // Apply headers
+        header('Content-Type: application/json');
+        if( isset($responseHeaders[self::$txId]) )
+        {
+            header("{$txName}: {$responseHeaders[self::$txId]}");
+        }
+
+        // Print result
+        print $responseBody;
+    }
+}
